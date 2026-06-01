97250ba24a5cf3450cf316ca2192b31db333efad
Adenle Samuel <adexsammy@gmail.com>
2026-06-01 14:56:08 +0100

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,21 @@
-import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { QueryClient } from "@tanstack/react-query";
 
-export const getRouter = () => {
+export function createRouter() {
   const queryClient = new QueryClient();
-
-  const router = createRouter({
+  
+  const router = createTanStackRouter({
     routeTree,
     context: { queryClient },
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
+    defaultPreload: "intent",
   });
 
   return router;
-};
+}
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
+}
